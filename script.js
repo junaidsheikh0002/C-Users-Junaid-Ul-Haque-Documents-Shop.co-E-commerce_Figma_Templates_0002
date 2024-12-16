a107b977c0e02edc5b5d67095d5f865eae3ba9fe
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-12-16 15:49:02 +0500

    https://github.com/junaidsheikh0002/C-Users-Junaid-Ul-Haque-Documents-Shop.co-E-commerce_Figma_Templates_0002/upload
    https://github.com/junaidsheikh0002/C-Users-Junaid-Ul-Haque-Documents-Shop.co-E-commerce_Figma_Templates_0002/upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple Interactivity for Cart
+const cartButton = document.querySelector('.shop-now');
+cartButton.addEventListener('click', () => {
+    alert('Redirecting to shop...');
+});
+
+// Example for filtering
+console.log("Filters, sorting, and cart can be extended here!");
